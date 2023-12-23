4552c118528408b45d95dfda30e68b724869cd58
Naman Pancholi <mahendra.naman07@gmail.com>
2023-12-23 14:56:28 +0530

    Create ocr model

---
+++ models/ocr.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const ocrSchema = new Schema({
+        status: {
+            type: String,
+            enum: ['SUCCESS', 'FAILURE'],
+            default: 'SUCCESS',
+        },
+        identification_number: {
+            type: String,
+        },
+        name: {
+            type: String,
+        },
+        last_name: {
+            type: String,
+        },
+        date_of_birth: {
+            type: Date,
+        },
+        date_of_issue: {
+            type: Date,
+        },
+        date_of_expiry: {
+            type: Date,
+        }
+},
+{timestamps: true}
+);
+
+const OCR = model('ocr', ocrSchema);
+
+module.exports = OCR;
