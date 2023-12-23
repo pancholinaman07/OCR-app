89476fc643944a9e326eb7450273c4c0facacd2e
Naman Pancholi <mahendra.naman07@gmail.com>
2023-12-23 22:24:19 +0530

    created editrecord handler

--- controllers/record.js
+++ controllers/record.js
@@ -10,19 +10,19 @@ async function handleRenderAddRecord(req, res) {
 // POST /record/add redirect to /record/record id
 async function handleAddRecord(req, res) {
     //todo testing right now
-    // const record = await Record.create({
-    //     inputImageURL:  `/uploads/${req.file.filename}`,
-    //     identification_number: '1111',
-    //     name: 'naman',
-    //     last_name: 'pancholi',
-    //     date_of_birth: '30-07-2001',
-    //     date_of_issue: '20-07-2003',
-    //     date_of_expiry: '20-07-2019'
-    // });
     const record = await Record.create({
-            inputImageURL:  `/uploads/${req.file.filename}`,
-            status: 'FAILURE'
-        })
+        inputImageURL:  `/uploads/${req.file.filename}`,
+        identification_number: '1111',
+        name: 'naman',
+        last_name: 'pancholi',
+        date_of_birth: '30-07-2001',
+        date_of_issue: '20-07-2003',
+        date_of_expiry: '20-07-2019'
+    });
+    // const record = await Record.create({
+    //         inputImageURL:  `/uploads/${req.file.filename}`,
+    //         status: 'FAILURE'
+    //     })
     return res.redirect(`/record/${record._id}`);
 }
 
@@ -52,13 +52,29 @@ async function handleRenderEditRecord(req, res) {
 
 // POST /record/edit/record:id redirect to /record/record id
 async function handleEditRecord(req, res) {
-    return res.json({error: 'EditRecord'});
+    try {
+        const {identification_number, name, last_name, date_of_birth, date_of_issue, date_of_expiry} = req.body;
+        const record = await Record.findByIdAndUpdate(req.params.recordId,{
+                identification_number,
+                name,
+                last_name,
+                date_of_birth,
+                date_of_issue,
+                date_of_expiry
+            });
+        return res.redirect(`/record/${req.params.recordId}`);
+    }catch (error) {
+        return res.render('404');
+    }
 }
 
 // GET /record/history
 // GET /record/record id
 async function handleRenderHistory(req, res) {
-    return res.json({error: 'History'});
+    const records = await Record.find({});
+    return res.render('history', {
+        records: records
+    });
 }
 
 module.exports = {
