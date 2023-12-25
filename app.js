ec66fc3abf1a7ed802834965862bcd3e27e8807c
Naman Pancholi <mahendra.naman07@gmail.com>
2023-12-25 11:18:56 +0530

    added comments in app.js and refactored;

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
+// Environmental variable loaded
 require('dotenv').config();
 
-const express = require('express');
-const mongoose = require('mongoose');
 const path = require("path");
 
+// Express Frame Work
+const express = require('express');
+
+// Routes
 const recordRoute = require('./routes/record');
+
+// Config
+const {connectMongoDb} = require("./config/connection");
+
+// Middlewares
 const {logReqRes} = require("./middlewares/logger");
 
+// Initialise the app
 const app = express();
+
+// Get the PORT
 const PORT = process.env.PORT || 8000;
 
-mongoose
-    .connect(`mongodb+srv://pancholinaman:pancholinaman@cluster0.7zc51v4.mongodb.net/qoala?retryWrites=true&w=majority`)
+// Connect To Database
+connectMongoDb(`mongodb+srv://pancholinaman:pancholinaman@cluster0.7zc51v4.mongodb.net/qoala?retryWrites=true&w=majority`)
     .then((e) => console.log('MongoDB Connected'));
 
+
+// Set Views
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
+
+// Middlewares added
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.resolve('./public')));
 app.use(logReqRes("logs/logs.txt"));
 
+// Routes added
 app.get('/', async (req, res) => {
     return  res.render('home');
 });
-
 app.use('/record', recordRoute);
 
+
+// Start the server
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
