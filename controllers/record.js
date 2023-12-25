fb073771603423a7a3b7e6cbff399c7a9a55aed3
Naman Pancholi <mahendra.naman07@gmail.com>
2023-12-25 12:55:35 +0530

    gitignore updated

--- controllers/record.js
+++ controllers/record.js
@@ -2,7 +2,6 @@ const Record = require('../models/record');
 const { extract } = require('../services/extract');
 const fs = require('fs');
 const { getData } = require('../services/ocr_api');
-const path = require("path");
 
 
 // POST /record/add
@@ -12,7 +11,7 @@ async function handleAddRecord(req, res) {
     try{
 
         // Google API Call
-        const text = await getData(`./public/uploads/${req.file.filename}`, path.resolve('./cred/secret.json'));
+        const text = await getData(`./public/uploads/${req.file.filename}`, './secret.json');
 
         // Process and extract relevant information
         const extracted = extract(text);
