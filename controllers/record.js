e442c9b68f495b8993cf0608858d60abca000242
Naman Pancholi <mahendra.naman07@gmail.com>
2023-12-25 10:24:31 +0530

    Refactored and added comments to controllers

--- controllers/record.js
+++ controllers/record.js
@@ -8,30 +8,41 @@ const { getData } = require('../services/ocr_api');
 // Take the image convert to text than extraction and after saved to database
 async function handleAddRecord(req, res) {
 
-    const text = await getData(`./public/uploads/${req.file.filename}`, './secret.json');
+    try{
 
-    const extracted = extract(text);
+        // Google API Call
+        const text = await getData(`./public/uploads/${req.file.filename}`, './secret.json');
 
-    const record = await Record.create({
-        status: extracted.status,
-        inputImageURL:  `/uploads/${req.file.filename}`,
-        identification_number: extracted.identification_number,
-        name: extracted.name,
-        last_name: extracted.last_name,
-        date_of_birth: extracted.date_of_birth,
-        date_of_issue: extracted.date_of_issue,
-        date_of_expiry: extracted.date_of_expiry
-    });
+        // Process and extract relevant information
+        const extracted = extract(text);
 
-    return res.redirect(`/record/${record._id}`);
+        // Added path of saved image
+        extracted.inputImageURL = `/uploads/${req.file.filename}`;
+
+        // Save to database
+        const record = await Record.create(extracted);
+
+        // Redirect to record page
+        return res.redirect(`/record/${record._id}`);
+
+    }catch (error) {
+
+        // Any error redirect to 404
+        console.log('error while edit record', error);
+        return res.render('404');
+
+    }
 }
 
 // POST /record/edit/:recordId
 // Update the database with Updated Info
 async function handleEditRecord(req, res) {
     try {
+
+        // Get the updated info as POST
         const {identification_number, name, last_name, date_of_birth, date_of_issue, date_of_expiry} = req.body;
 
+        // Update in DataBase
         const record = await Record.findByIdAndUpdate(req.params.recordId,{
                 identification_number,
                 name,
@@ -41,9 +52,13 @@ async function handleEditRecord(req, res) {
                 date_of_expiry
             });
 
+        // Redirect to Record page
         return res.redirect(`/record/${req.params.recordId}`);
 
     }catch (error) {
+
+        // Any error redirect to 404
+        console.log('error while edit record', error);
         return res.render('404');
     }
 }
@@ -52,13 +67,26 @@ async function handleEditRecord(req, res) {
 // delete the record
 async function handleDeleteRecord(req, res) {
     try{
+
+        // Find the record and delete in database
         const record =await Record.findByIdAndDelete(req.params.recordId);
+
+        // Delete the image from local storage
         fs.unlink(`./public/${record.inputImageURL}`, () => {});
+
+        // Redirect to Home Page
         return res.redirect(`/`);
+
     }catch (error) {
+
+        // Any error redirect to 404
+        console.log('error while edit record', error);
         return res.render('404');
+
     }
 }
+
+// Exports
 module.exports = {
     handleAddRecord,
     handleEditRecord,
